6dfcb83c94fe5d713d7e6c5a37f893743154b2fb
Naman Kumar <namank2002@gmail.com>
2018-05-26 11:37:39 +0530

    q14 solved
    Co-Authored-By: Dhruv Jain <39631663+arcanemagic@users.noreply.github.com>

---
+++ q14.py
@@ -0,0 +1,18 @@
+num = int(input("Enter number to check: "))
+
+m = num
+l = 0
+while(m>0):
+    l += 1
+    m = m//10
+
+rev = 0
+
+for i in range(l, 0, -1):
+    rev += ((num%(10**i) - num%(10**(i-1)))/(10**(i-1))) * (10**(l-i))
+
+
+if(int(rev) == num):
+    print("It's a palindrome!")
+else:
+    print("It's not a palindrome")
