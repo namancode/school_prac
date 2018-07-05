a3336cae446bc0c472186f21dbdd2a95240b29e0
Naman Kumar <namank2002@gmail.com>
2018-07-05 13:41:45 +0530

    Question 20 solved

---
+++ Section A/q20.py
@@ -0,0 +1,18 @@
+import math
+
+t = int(input("Enter number of rows: "))
+
+def combination(n, r):
+    return int((math.factorial(n)) / ((math.factorial(r)) * math.factorial(n - r)))
+
+def pascals_triangle(rows):
+    result = []
+    for count in range(rows):
+        row = []
+        for element in range(count + 1):
+            row.append(combination(count, element))
+        result.append(row)
+    return result
+
+for row in pascals_triangle(t):
+    print(row)
