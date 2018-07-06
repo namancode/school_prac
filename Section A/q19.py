ff9b397327a7292e08725875bbdd5552c851d4c3
Naman Kumar <namank2002@gmail.com>
2018-07-06 13:49:00 +0530

    Solved

---
+++ Section A/q19.py
@@ -0,0 +1,12 @@
+"""
+Question 19
+"""
+
+print("Input date:")
+d = int(input("Enter day: "))
+m = int(input("Enter month: "))
+y = int(input("Enter year: "))
+
+mon = {1:"JAN", 2:"FEB", 3:"MAR", 4:"APR", 5:"MAY", 6:"JUN", 7:"JUL", 8:"AUG", 9:"SEP", 10:"OCT", 11:"NOV", 12:"DEC"}
+
+print(str(d) + "-" + mon[m] + "-" + str(y))
